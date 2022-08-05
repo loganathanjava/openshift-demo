9cfca095b84317ff6643a1991f2958a15ee52631
Loganathan P <loganathan251293@gmail.com>
2022-08-05 10:34:53 +0530

    Commit

--- src/main/java/com/logan/openshiftdemo/controller/DemoController.java
+++ src/main/java/com/logan/openshiftdemo/controller/DemoController.java
@@ -2,13 +2,16 @@ package com.logan.openshiftdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class DemoController {
 
 	@GetMapping("/")
-	public String ping() {
-		return "Demo service is working properly: " + System.currentTimeMillis();
+	public ModelAndView ping() {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("index");
+		return view;
 	}
 	
 }
